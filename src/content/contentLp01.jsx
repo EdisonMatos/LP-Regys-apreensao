94d6cc648673fcd0dfa5e200da3705a39e507f93
Edison Matos <edison.matos@live.com>
2025-01-20 09:03:44 -0300

    Initial commit

---
+++ src/content/contentLp01.jsx
@@ -0,0 +1,346 @@
+import bgImg from "../assets/imgs/hero/bgHeroLp01.webp";
+import logo from "../assets/imgs/logo/logoLp.png";
+import { infos } from "./content";
+
+const currentYear = new Date().getFullYear();
+
+const contentLp01 = {
+  infos: {
+    title: "Regys Fernando - Recuperação de Veículos Apreendidos",
+    description:
+      "Busca e Apreensão de Veículos, Recuperação de Veículos Apreendidos, Defesa de Veículos Apreendidos, Advogado para Recuperação de Veículos, Veículo Apreendido, Recuperação de Bem Apreendido, Advogado de Busca e Apreensão, Ações de Busca e Apreensão, Processo de Recuperação de Veículos, Defesa Patrimonial, Reverter Apreensão de Veículo, Advogado para Veículo Apreendido, Recuperação Rápida de Veículos, Reverter Busca e Apreensão, Proteção de Veículo, Ajuda Jurídica para Apreensão de Veículo, Advogado Especializado em Recuperação de Veículos, Recuperação Judicial de Veículos, Soluções Jurídicas para Apreensão de Veículos, Consultoria Jurídica de Veículos Apreendidos, Recuperação de Carros Apreendidos.",
+  },
+  hero: {
+    bgImg: bgImg,
+    logo: {
+      logoImg: logo,
+      alt: "Logomarca",
+    },
+    textArea: {
+      title: (
+        <h1>
+          Não deixe seu veículo{" "}
+          <span className="text-primary"> ser levado! </span> Recupere-o
+          <span className="text-primary"> antes que seja tarde </span> demais!
+        </h1>
+      ),
+      subtitle:
+        "Sofreu com busca e apreensão de veículo? Nosso time de especialistas está aqui para ajudar você a recuperar seu bem com segurança e assertividade. Não perca tempo, cada minuto conta para proteger o que é seu.",
+      buttonLabel: "Recuperar meu veículo agora",
+      ctaButtonAriaLabel:
+        "Botão para chamada de ação para contato pelo whatsapp",
+    },
+  },
+  features: {
+    sectionHeader: {
+      miniTag: "SOLUÇÕES PARA BUSCA E APREENSÃO DE VEÍCULOS",
+      title:
+        "Descubra se você pode evitar ou reverter a busca e apreensão do seu veículo!",
+    },
+    cards: {
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-hand-coins"
+          >
+            <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
+            <path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
+            <path d="m2 16 6 6" />
+            <circle cx="16" cy="9" r="2.9" />
+            <circle cx="6" cy="5" r="3" />
+          </svg>
+        ),
+        title: "Proprietários com financiamento em atraso",
+        description:
+          "Se você está com parcelas do financiamento em atraso, saiba que ainda há chances de evitar a apreensão do seu veículo. Nós analisamos seu caso e agimos rapidamente para proteger seu patrimônio.",
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-car"
+          >
+            <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+            <circle cx="7" cy="17" r="2" />
+            <path d="M9 17h6" />
+            <circle cx="17" cy="17" r="2" />
+          </svg>
+        ),
+        title: "Empresas com frota financiada",
+        description:
+          "Empresas que dependem de sua frota para operar não podem se dar ao luxo de perder veículos. Oferecemos soluções personalizadas para evitar a busca e apreensão, garantindo a continuidade dos seus negócios.",
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-triangle-alert"
+          >
+            <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3" />
+            <path d="M12 9v4" />
+            <path d="M12 17h.01" />
+          </svg>
+        ),
+        title: "Pessoas que já tiveram o veículo apreendido",
+        description:
+          "Se seu veículo já foi apreendido, ainda há tempo para agir. Nossa equipe especializada trabalha para recuperar o bem e reverter a situação, minimizando os danos e protegendo seus direitos.",
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-check"
+          >
+            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="m9 15 2 2 4-4" />
+          </svg>
+        ),
+        title: "Gestores preocupados com a regularização de veículos",
+        description:
+          "Gestores que desejam manter a frota em conformidade e evitar problemas futuros podem contar com nossa assessoria. Garantimos a regularização dos veículos e a segurança jurídica necessária para operar sem riscos.",
+      },
+    },
+  },
+  about: {
+    bgImg: bgImg,
+    logo: {
+      logoImg: logo,
+      alt: "Logomarca",
+    },
+    sectionHeader: {
+      miniTag: "EXPERTISE EM RECUPERAÇÃO DE VEÍCULOS APREENDIDOS",
+      title: "Não perca seu veículo! Recupere-o com quem entende do assunto",
+    },
+    paragraph: (
+      <p>
+        Com ampla experiência em questões jurídicas relacionadas à busca e
+        apreensão, nossa equipe é especializada em reverter a apreensão de
+        veículos. <br />
+        <br /> Atuamos rapidamente para proteger seu patrimônio e garantir que
+        seus direitos sejam respeitados, oferecendo soluções eficazes e seguras.
+        Conte com a nossa expertise para recuperar seu veículo e restaurar a
+        normalidade da sua vida.
+      </p>
+    ),
+    buttonLabel: "Quero falar com um especialista",
+    ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
+  },
+  whyUs: {
+    sectionHeader: {
+      miniTag: "A MELHOR ESCOLHA",
+      title: "Por que escolher o Regys Fernando Advocacia e Consultoria?",
+    },
+    cards: {
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-scale"
+          >
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+            <path d="M7 21h10" />
+            <path d="M12 3v18" />
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2" />
+          </svg>
+        ),
+        title: "Especialização Jurídica",
+        description:
+          "Com a experiência do Dr Regys Fernando, analisamos profundamente cada caso para garantir a recuperação dos seus direitos, com total conformidade legal.",
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-chart-column-increasing"
+          >
+            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="M8 18v-2" />
+            <path d="M12 18v-4" />
+            <path d="M16 18v-6" />
+          </svg>
+        ),
+        title: "Análise Inicial",
+        description:
+          "Oferecemos uma avaliação detalhada da sua situação, sem compromisso, para ajudar a entender os próximos passos no processo de recuperação do seu veículo.",
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-history"
+          >
+            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+            <path d="M3 3v5h5" />
+            <path d="M12 7v5l4 2" />
+          </svg>
+        ),
+        title: "Agilidade e Transparência",
+        description:
+          "Com uma abordagem rápida e eficiente, você receberá atualizações constantes em cada fase do processo, sem surpresas ou custos ocultos.",
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-shield-check"
+          >
+            <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
+            <path d="m9 12 2 2 4-4" />
+          </svg>
+        ),
+        title: "Recuperação e Proteção",
+        description:
+          "O escritório Regys Fernando Advocacia e Consultoria trabalha para garantir que seu veículo seja recuperado de forma legal e segura. Conte com nossa expertise para restaurar sua tranquilidade e proteger seus bens.",
+      },
+    },
+  },
+  cta: {
+    bgImg: bgImg,
+    sectionHeader: {
+      miniTag: "NÃO DEIXE SEU VEÍCULO PERDIDO",
+      title: "Recupere seu veículo agora!",
+      subtitle: `Não deixe seu veículo ser levado sem lutar. Nossa equipe especializada pode ajudá-lo a reverter a apreensão e proteger seu patrimônio. A análise inicial é gratuita, então não perca tempo.`,
+    },
+    buttonLabel: "Recuperar meu veículo agora",
+    ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
+  },
+  contact: {
+    card1: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-instagram"
+        >
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+          <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+        </svg>
+      ),
+      title: "Instagram",
+      description: `@${infos.instagramProfile}`,
+    },
+    card2: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-mail"
+        >
+          <rect width="20" height="16" x="2" y="4" rx="2" />
+          <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+        </svg>
+      ),
+      title: "Email",
+      description: `${infos.email}@${infos.domain}`,
+    },
+    card3: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          className="bi bi-whatsapp"
+          viewBox="0 0 16 16"
+        >
+          <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+        </svg>
+      ),
+      title: "Whatsapp",
+      description: `(${infos.phone.ddd}) ${infos.phone.firstPart}-${infos.phone.secondPart}`,
+    },
+  },
+  footer: {
+    copyrightLine: `© ${currentYear} ${infos.name}. Todos os direitos reservados.`,
+    disclaimer: `Este site não é um produto Meta Platforms, Inc., Google LLC, tampouco oferece serviços públicos oficiais. ${infos.name} oferece serviços jurídicos privativos de advogado, de acordo com a legislação vigente e o Código de Ética e Disciplina da Ordem dos Advogados do Brasil.`,
+  },
+  links: {
+    instagram: `https://www.instagram.com/${infos.instagramProfile}/`,
+    ctaWhatsapp: `https://wa.me/+55${infos.phone.ddd}${infos.phone.firstPart}${infos.phone.secondPart}?text=${infos.whatsappDefaultMessage}`,
+  },
+};
+
+export default contentLp01;
